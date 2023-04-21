17de93ca5650ccdc20c609bb941a85683c2ab9a3
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-04-21 12:58:03 -0300

    :lipstick: feat: calculate the distance using mouse events

--- js/slide.js
+++ js/slide.js
@@ -2,19 +2,33 @@ export class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.5;
+    return (this.dist.finalPosition - this.dist.movement) ;
+  }
+  
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener('mousemove',this.onMove);
   }
 
   onMove(event) {
-    console.log('moving');
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition)
   }
 
   onEnd() {
     this.wrapper.removeEventListener('mousemove',this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
